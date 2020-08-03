e8993c7478b9bb10cfa405f3e1425eb1dfb430a2
Dane Johnson <dane_johnson@ymail.com>
2020-08-03 10:57:05 -0500

    Adde std lib as a submodule

--- compile.lua
+++ compile.lua
@@ -1,5 +1,5 @@
 #!/usr/bin/lua
 fennel = require('fennel')
 local source = io.open("src/main.fnl", "r"):read("a")
-fennel.path = fennel.path .. ";./src/?.fnl"
+fennel.path = fennel.path .. ";./src/?.fnl;./fennel-std-library/?.fnl"
 print(fennel.compileString(source))
